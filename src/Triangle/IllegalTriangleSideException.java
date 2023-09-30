3d9589d9c1fba7afcfc82eacf85b718a5bf43bb1
WilliamChokbengboune <wchokbengboune@ggc.edu>
2023-09-29 21:20:43 -0400

    Exception class has been created

---
+++ src/Triangle/IllegalTriangleSideException.java
@@ -0,0 +1,15 @@
+package Triangle;
+
+/**
+ * Class: IllegalTriangleSideException
+ * Author: William Chokbengboune
+ * Date: September 29, 2023
+ * Description: This is the exception class that is referred to
+ */
+
+
+public class IllegalTriangleSideException  extends Exception{
+    IllegalTriangleSideException(String message){
+        super(message);
+    };
+}
--- src/Triangle/Triangle.java
+++ src/Triangle/Triangle.java
@@ -4,8 +4,61 @@ package Triangle;
  * Class: Triangle
  *Author: William Chokbengbooune
  * Date: 9/29/2023
+ * Description: Triangle checks the sides of a and bto make sure they are bigger than c
  *
  */
 
 public class Triangle {
+    private double sideA;
+    private double sideB;
+    private double sideC;
+
+    public Triangle(){
+    }
+
+    public double getSideA() {
+        return sideA;
+    }
+
+    public double getSideB() {
+        return sideB;
+    }
+
+    public double getSideC() {
+        return sideC;
+    }
+
+    boolean checkSides(double sideA, double sideB, double sideC) throws IllegalTriangleSideException{
+        if ((sideA + sideB > sideC) && (sideB + sideC > sideA) && (sideA + sideC > sideB) ){
+            System.out.println("This is a triangle.");
+            return true;
+        }
+        else {
+            throw new IllegalTriangleSideException ("Side C is less than A and B combined.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Triangle te1 = new Triangle();
+        Triangle te2 = new Triangle();
+        Triangle te3 = new Triangle();
+        try {
+            boolean result = te1.checkSides(1.5, 6.8, 3.2);
+        }catch (IllegalTriangleSideException ae){
+            System.out.println(ae.getMessage());
+        }
+
+        try {
+            boolean result = te2.checkSides(1.5, 0.8, 6.2);
+        }catch (IllegalTriangleSideException ae){
+            System.out.println(ae.getMessage());
+        }
+
+        try {
+            boolean result = te3.checkSides(1.5, 1.1, 2.6);
+        }catch (IllegalTriangleSideException ae){
+            System.out.println(ae.getMessage());
+        }
+    }
 }
+
